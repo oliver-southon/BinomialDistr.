21e79ab6f1ae7aab3667c95244b5bc33a68cd8a4
Oliver <oliversouthon@gmail.com>
2021-03-24 16:17:06 +1100

    second-commit

--- binomial.py
+++ binomial.py
@@ -1,49 +1,62 @@
 # PURPOSE: Take in n, p, k vals and performs a binomial calculation to solve a problem
-import numpy as np
+from math import factorial as fac
+import PySimpleGUIQt as sg
 
-def factorial(n):
-    result = 1
-    for i in range(1,n+1):
-        result *= i
-    return result
+def binomial(n,p,k):
+    prob = fac(n) / (fac(k) * fac(n - k)) * p**k * (1-p)**(n-k)
+    return prob
 
-def binomial(n,p,k, op):
-    q = round(1 - p,4)
-    choose = (factorial(n)/(factorial(k)*factorial(n-k)))
+def calcProb(n,p,k,op):
+    prob = 0
     if op == '=':
-        prob = round(float(choose * (p ** k) * (q ** (n-k))),4)
+        prob = binomial(n,p,k)
     elif op == '<':
         prob = 0
         for i in range(0,k):
-            prob += round(float(choose * (p ** k) * (q ** (n-k))),4)
+            prob += binomial(n,p,i)
     elif op == '<=':
-        prob = 0
         for i in range(0,k+1):
-            prob += round(float(choose * (p ** k) * (q ** (n-k))),4)
+            prob += binomial(n,p,i)
     elif op == '>':
         prob = 0
-        for i in range(k+1,n):
-            prob += round(float(choose * (p ** k) * (q ** (n-k))),4)
+        for i in range(k+1,n+1):
+            prob += binomial(n,p,i)
     elif op == '>=':
-        print('greater than or equal to')
         prob = 0
         for i in range(k,n+1):
-            print(i)
-            print(str(float(choose * (p ** i) * (q ** (n-i)))))
-            prob += float(choose * (p ** i) * (q ** (n-i)))
+            prob += binomial(n,p,i)
     return prob
 
-try:
-    n = int(input("Enter the sample space: "))
-    p = float(input("Enter the probability of success: "))
-    k = int(input("Enter how many successes: "))
-    op = input("Enter an operation: ")
-    ans = binomial(n,p,k, op)
-    print("The answer is: " + str(ans))
-except ValueError:
-    print('ERROR: Incorrect value type inputted.')
-# print(binomial(5,0.8,3))
-# print("%0.1f" % (1.0-0.8))
 
+r_keys= ['=','<','<=','>','>=']
+sg.change_look_and_feel('DarkBlue1') #colour
+layout = [
+    [sg.Text('| -N- | Sample space:'), sg.InputText(key='-N-')],
+    [sg.Text('| -P- | Success rate: '), sg.InputText(key='-P-')],
+    [sg.Text('| -K- | Number of successes:'), sg.InputText(key='-K-')],
+    [sg.Text('Choose an operation: ')],
+    [sg.Radio('=', 1, key=r_keys[0], default = True),
+        sg.Radio('<', 1, key=r_keys[1]),
+        sg.Radio('<=', 1, key=r_keys[2]),
+        sg.Radio('>', 1, key=r_keys[3]),
+        sg.Radio('>=', 1, key=r_keys[4])],
+    [sg.Button('Calculate'), sg.Button('Clear'), sg.Button('Exit')],
+    [sg.Output(size=(30,10), key='_output_')]
+]
+window = sg.Window('Binomial Calculator', layout) #makes window
 
+while True:
+    event, values = window.read()
+    n = int(values['-N-'])
+    p = float(values['-P-'])
+    k = int(values['-K-'])
+    op = [ key for key in r_keys if values[key]][0]
 
+    if event in (None, 'Exit'):
+        window.close()
+    if n>0 and p>0 and k>0 and op>0:
+        print(calcProb(n,p,k,op))
+    if event == 'Clear':
+        window.FindElement('_output_').Update('') #clears element
+
+ 
